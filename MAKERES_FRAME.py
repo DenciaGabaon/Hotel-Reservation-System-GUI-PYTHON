c47780ec42c76656d1790a002782ed70cafd0946
DenciaGabaon <gabaondenden@gmail.com>
2023-06-30 18:55:38 +0800

    not working

---
+++ MAKERES_FRAME.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from PIL import Image, ImageTk
+from MENUFRAME import *
+
+
+main_frame = Frame(menuWindow, bd=0)
+main_frame.place(x=322, y=211, width=830, height=475)
+
+test = Label(text = "test hotel info frame", font=('tahoma', 40, 'bold'), fg='white', bg='black')
+test.place(x=300, y=250)
