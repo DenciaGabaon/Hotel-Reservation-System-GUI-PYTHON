34ba47390214ecfe936e67ededb4ec4a21723504
DenciaGabaon <gabaondenden@gmail.com>
2023-06-30 19:03:24 +0800

    LOGIN LOGIC & DESIGN, MENU DESIGN DONE

--- HRM GUI.py
+++ HRM GUI.py
@@ -1,113 +1,50 @@
 from tkinter import *
 import tkinter as tk
-from menu import *
-from PIL import Image,ImageTk
+import os
+from PIL import Image, ImageTk
 from PIL import Image, ImageDraw
 from tkinter import messagebox
 
-
-def menu_Window(): #FUNCTION FOR MENU
-    Menu = Tk()
+def open_menu():
     main_window.destroy()
-
-    """
-    menu.overrideredirect(True)  # Remove window decorations
-    menu.configure(bg='black')  # Set background color of the entire window
-    windowx_width = 1000  # define window dimensions width and height
-    windowx_height = 700
-    screenx_width = main_window.winfo_screenwidth()  # get the screen size of your computer
-    screenx_height = main_window.winfo_screenheight()
-
-    positionx_top = int(screen_height / 2.5 - window_height / 2.5)  # Get the window position from the top dynamically
-    positionx_right = int(screen_width / 2 - window_width / 2)  # as well as position from left or right as follows
-
-    menu.geometry(
-        f'{windowx_width}x{windowx_height}+{positionx_right}+{positionx_top}')  # this is the line that will center your window
-
-    """
-
-    # Create a custom title bar
-    title = Frame(main_window, bg='#EEBA2B', height=30)
-    title.configure(height=38)
-    title.pack(fill=X)
-
-    # Add title label to the title bar
-    title_lbl = Label(title_bar, text='Picadili Travels : Hotel Reservation System', fg='white', bg='#EEBA2B')
-    title_lbl.place(x=10, y=10)
-
-    # Add close button to the title bar
-    close_bttn = Button(title_bar, text='X', font=('Tahoma', 10, 'bold'), width=1, command=close_window, bg='#EEBA2B')
-    close_bttn.pack(side=RIGHT)
-
-    Menu.mainloop()
+    import MENUFRAME
 
 
 def xor_encrypt_decrypt(data, key):
     encrypted_data = "".join(chr(ord(c) ^ key) for c in data)
     return encrypted_data
 
+
 def login():
-  username = entry_username.get()
-  password = entry_password.get()
+    username = entry_username.get()
+    password = entry_password.get()
 
-  try:
-      # Read the encrypted credentials from the file
-      count = 0
-      with open("admin.txt") as fp:
-          encrypted_username = fp.readline().strip()
-          encrypted_password = fp.readline().strip()
+    file_path = "admin.txt"
+    if os.path.isfile(file_path):
+        # Read the encrypted credentials from the file
+        with open("admin.txt") as fp:
+            encrypted_username = fp.readline().strip()
+            encrypted_password = fp.readline().strip()
 
-      #print("username: ", encrypted_username) TEST
-      #print("password: ", encrypted_password) TEST
+        # print("username: ", encrypted_username) TEST
+        # print("password: ", encrypted_password) TEST
 
-      key = 73
+        key = 73
 
-      # Decrypt the message
-      decrypted_user = xor_encrypt_decrypt(encrypted_username, key)
-      decrypted_pass = xor_encrypt_decrypt(encrypted_password, key)
+        # Decrypt the message
+        decrypted_user = xor_encrypt_decrypt(encrypted_username, key)
+        decrypted_pass = xor_encrypt_decrypt(encrypted_password, key)
 
-      # Switching from main window to menu
-      mainWindow = tk.Tk()
-      mainWindow.title('Login Successful')
-
-      # Center the mainWindow on the screen
-      window_width = 300
-      window_height = 150
-      screen_width = mainWindow.winfo_screenwidth()
-      screen_height = mainWindow.winfo_screenheight()
-      x_pos = (screen_width // 2) - (window_width // 2)
-      y_pos = (screen_height // 2) - (window_height // 2)
-      mainWindow.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
-
-      text1 = Text(mainWindow, height=1, width=50)
-      text1.tag_configure("center", justify='center')
-
-      welcome_text = "Welcome, " + username + "!"
-      text1.insert(tk.END, welcome_text, "center")
-
-      text1.config(state="disabled")
-
-      text1.pack()
-
-      def open_menu():
-          menu_window()
-
-      # Check if the decrypted username and password match the entered values
-      if decrypted_user == username and decrypted_pass == password:
-       #   messagebox.showinfo("Login Successful", "Welcome, " + username + "!")
-          btn1 = Button(mainWindow, text='Ok!', bd=3, command=open_menu)
-          btn1.pack()
-      elif username == "admin" and password == "password":
-        #  messagebox.showinfo("Login Successful", "Welcome, " + username + "!")
-          btn1 = Button(mainWindow, text='Ok', bd=3, command=open_menu)
-          btn1.pack()
-
-          mainWindow.mainloop()
-
-      else:
-          messagebox.showerror("Login Failed", "Invalid username or password")
-  except FileNotFoundError:
-       status_label.config(text="Credentials not found")
+        # Check if the decrypted username and password match the entered values
+        if decrypted_user == username and decrypted_pass == password:
+            messagebox.showinfo("Login Successful", "Welcome, " + username + "!")
+            open_menu()
+        elif username == "admin" and password == "password":
+            messagebox.showerror("Login Failed", "Invalid username or password")
+        else:
+            messagebox.showerror("Login Failed", "Invalid username or password")
+    else:
+        status_label.config(text="Credentials not found")
 
 
 def on_username_focus_in(event):
@@ -115,23 +52,26 @@ def on_username_focus_in(event):
         entry_username.delete(0, END)
         entry_username.config(fg="black")
 
+
 def on_username_focus_out(event):
     if entry_username.get() == "":
         entry_username.insert(0, "Username")
         entry_username.config(fg="gray")
+
 
 def on_password_focus_in(event):
     if entry_password.get() == "Password":
         entry_password.delete(0, END)
         entry_password.config(show="*", fg="black")
 
+
 def on_password_focus_out(event):
     if entry_password.get() == "":
         entry_password.insert(0, "Password")
         entry_password.config(show="", fg="gray")
 
-def roundPolygon(x, y, sharpness, **kwargs):
 
+def roundPolygon(x, y, sharpness, **kwargs):
     # The sharpness here is just how close the sub-points
     # are going to be to the vertex. The more the sharpness,
     # the more the sub-points will be closer to the vertex.
@@ -155,57 +95,58 @@ def roundPolygon(x, y, sharpness, **kwargs):
         if i != (len(x) - 1):
             # Insert submultiples points. The more the sharpness, the more these points will be
             # closer to the vertex.
-            points.append((ratioMultiplier*x[i] + x[i + 1])/ratioDividend)
-            points.append((ratioMultiplier*y[i] + y[i + 1])/ratioDividend)
-            points.append((ratioMultiplier*x[i + 1] + x[i])/ratioDividend)
-            points.append((ratioMultiplier*y[i + 1] + y[i])/ratioDividend)
+            points.append((ratioMultiplier * x[i] + x[i + 1]) / ratioDividend)
+            points.append((ratioMultiplier * y[i] + y[i + 1]) / ratioDividend)
+            points.append((ratioMultiplier * x[i + 1] + x[i]) / ratioDividend)
+            points.append((ratioMultiplier * y[i + 1] + y[i]) / ratioDividend)
         else:
             # Insert submultiples points.
-            points.append((ratioMultiplier*x[i] + x[0])/ratioDividend)
-            points.append((ratioMultiplier*y[i] + y[0])/ratioDividend)
-            points.append((ratioMultiplier*x[0] + x[i])/ratioDividend)
-            points.append((ratioMultiplier*y[0] + y[i])/ratioDividend)
+            points.append((ratioMultiplier * x[i] + x[0]) / ratioDividend)
+            points.append((ratioMultiplier * y[i] + y[0]) / ratioDividend)
+            points.append((ratioMultiplier * x[0] + x[i]) / ratioDividend)
+            points.append((ratioMultiplier * y[0] + y[i]) / ratioDividend)
             # Close the polygon
             points.append(x[0])
             points.append(y[0])
 
             return canvas.create_polygon(points, **kwargs, smooth=TRUE)
-            #return userlog.create_polygon(points, **kwargs, smooth=TRUE)
+            # return userlog.create_polygon(points, **kwargs, smooth=TRUE)
 
 
 def close_window():
     main_window.destroy()
 
+
 main_window = Tk()
 main_window.overrideredirect(True)  # Remove window decorations
 main_window.configure(bg='black')  # Set background color of the entire window
 
-#Image declaration
+# Image declaration
 suite = (Image.open("suite.png"))
 suite_resize = suite.resize((400, 500), Image.LANCZOS)
 suite_img = ImageTk.PhotoImage(suite_resize)
 
 name = (Image.open("Picadiliname.png"))
 name_resize = name.resize((280, 60), Image.LANCZOS)
-name_img  = ImageTk.PhotoImage(name_resize)
+name_img = ImageTk.PhotoImage(name_resize)
 
 logox = (Image.open("Picadililogox.png"))
 logox_resize = logox.resize((85, 85), Image.LANCZOS)
 logox_img = ImageTk.PhotoImage(logox_resize)
 
-#========================Makes the main window center============================================
-window_width = 800   # define window dimensions width and height
+# ========================Makes the main window center============================================
+window_width = 800  # define window dimensions width and height
 window_height = 500
-screen_width = main_window.winfo_screenwidth()    # get the screen size of your computer
+screen_width = main_window.winfo_screenwidth()  # get the screen size of your computer
 screen_height = main_window.winfo_screenheight()
 
 position_top = int(screen_height / 2.5 - window_height / 2.5)  # Get the window position from the top dynamically
 position_right = int(screen_width / 2 - window_width / 2)  # as well as position from left or right as follows
 
-main_window.geometry(f'{window_width}x{window_height}+{position_right}+{position_top}') #this is the line that will center your window
+main_window.geometry(
+    f'{window_width}x{window_height}+{position_right}+{position_top}')  # this is the line that will center your window
 
-
-#Logo label
+# Logo label
 logo_label = Label(image=suite_img, borderwidth=0, highlightthickness=0)
 logo_label.place(x=0, y=0)
 
@@ -216,13 +157,13 @@ title_bar.pack(fill=X)
 
 # Add title label to the title bar
 title_label = Label(title_bar, text='Picadili Travels : Hotel Reservation System', fg='white', bg='#EEBA2B')
-title_label.place(x=10, y=10)
+title_label.place(x=10, y=4)
 
 # Add close button to the title bar
-close_button = Button(title_bar, text='X', font=('Tahoma', 10, 'bold'), width=1, command=close_window,bg='#EEBA2B' )
+close_button = Button(title_bar, text='X', font=('Tahoma', 10, 'bold'), width=1, command=close_window, bg='#EEBA2B')
 close_button.pack(side=RIGHT)
 
-#=================================use Images=======================================================
+# =================================use Images=======================================================
 logo_canvas = Canvas(main_window, width=400, height=125, borderwidth=0, highlightthickness=0)
 logo_canvas.create_rectangle(0, 0, 400, 200, fill="#0D0D0D")
 logo_canvas.place(x=0, y=165)
@@ -231,60 +172,43 @@ name_label.place(x=104, y=200)
 logox_label = Label(image=logox_img, borderwidth=0, highlightthickness=0, bg='#0D0D0D')
 logox_label.place(x=13, y=186)
 
-
-#====================================Log in Frame===================================================
-canvas = Canvas(main_window, width = 310, height = 380, borderwidth=0, highlightthickness=0, bg='black')
+# ====================================Log in Frame===================================================
+canvas = Canvas(main_window, width=310, height=380, borderwidth=0, highlightthickness=0, bg='black')
 my_rectangle = roundPolygon([8, 300, 300, 8], [3, 3, 375, 375], 5, width=3, outline="#272022", fill="#272022")
 canvas.place(x=445, y=73)
 
-
 login_label = Label(main_window, text="Welcome, Admin!",
-                  font=('Sans', 20, 'bold'),
-                  fg='white', bg='#272022', padx=0, pady=0)
+                    font=('Sans', 20, 'bold'),
+                    fg='white', bg='#272022', padx=0, pady=0)
 login_label.place(x=480, y=127)
 
-
-userlog = Canvas(main_window, width=250, height = 40, borderwidth=0, highlightthickness=0, bg='white')
-#rectlog = roundPolygon([5, 100, 100, 5], [3, 3, 30, 30], 5, width=3, outline="#FFFFFF", fill="#FFFFFF")
+userlog = Canvas(main_window, width=250, height=40, borderwidth=0, highlightthickness=0, bg='white')
+# rectlog = roundPolygon([5, 100, 100, 5], [3, 3, 30, 30], 5, width=3, outline="#FFFFFF", fill="#FFFFFF")
 userlog.place(x=473, y=190)
 
 entry_username = Entry(main_window, width=38, fg="gray")
-entry_username.config(relief= GROOVE, bd=0)
+entry_username.config(relief=GROOVE, bd=0)
 entry_username.insert(0, "Username")
 entry_username.bind("<FocusIn>", on_username_focus_in)
 entry_username.bind("<FocusOut>", on_username_focus_out)
 entry_username.place(x=490, y=201)
 
-passlog = Canvas(main_window, width=250, height = 40, borderwidth=0, highlightthickness=0, bg='white')
-#rectlog = roundPolygon([5, 100, 100, 5], [3, 3, 30, 30], 5, width=3, outline="#FFFFFF", fill="#FFFFFF")
+passlog = Canvas(main_window, width=250, height=40, borderwidth=0, highlightthickness=0, bg='white')
+# rectlog = roundPolygon([5, 100, 100, 5], [3, 3, 30, 30], 5, width=3, outline="#FFFFFF", fill="#FFFFFF")
 passlog.place(x=473, y=255)
 
 entry_password = Entry(main_window, width=38, fg="gray")
-entry_password.config(relief= GROOVE, bd=0)
+entry_password.config(relief=GROOVE, bd=0)
 entry_password.insert(0, "Password")
 entry_password.bind("<FocusIn>", on_password_focus_in)
 entry_password.bind("<FocusOut>", on_password_focus_out)
 entry_password.place(x=490, y=265)
 
-
-
 button_login = Button(main_window, text="LOGIN", font=('Sans', 12, 'bold'), command=login, width=38, bg='#EEBA2B')
 button_login.config(relief=GROOVE, bd=2, width=24)
 button_login.place(x=473, y=350)
 
+status_label = Label(main_window, text="", font=('Sans', 10), fg='Red', width=24, height=1, bg='#272022')
+status_label.place(x=450, y=313)
 
-
-status_label = Label(main_window, text="", font=('Sans', 12, 'bold'), fg='white', width=24, height=1, bg='#272022')
-status_label.place(x=473, y=313)
-
-
-
-main_window.mainloop() #runs main_window
-
-
-
-
-
-
-
-
+main_window.mainloop()  # runs main_window
