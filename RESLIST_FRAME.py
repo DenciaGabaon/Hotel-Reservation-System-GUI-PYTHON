30312125fac9a11f9dec21546e64a108fd4fe772
Daphne Camodoc <73266859+DaphneGwyneth@users.noreply.github.com>
2023-07-01 01:21:36 +0800

    nagana na

--- RESLIST_FRAME.py
+++ RESLIST_FRAME.py
@@ -2,9 +2,11 @@ from tkinter import *
 from PIL import Image, ImageTk
 from MENUFRAME import *
 
-main_frame = Frame(menuWindow, bd=0)
-main_frame.place(x=322, y=211, width=830, height=475)
+class ResListFrame(Frame):
+    def __init__(self, parent):
+        super().__init__(parent, bd=0, bg='white', width=830, height=475)
+        self.place(x=322, y=211)
 
+        test = Label(self, text="test res list frame", font=('tahoma', 40, 'bold'), fg='white', bg='black')
+        test.place(x=300, y=100)
 
-test = Label(text = "test hotel info frame", font=('tahoma', 40, 'bold'), fg='white', bg='black')
-test.place(x=300, y=400)
