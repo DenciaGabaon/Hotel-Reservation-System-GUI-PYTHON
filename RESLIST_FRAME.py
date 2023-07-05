b83f3baf61a00fe935da50aadf88fc77847fc450
Daphne Camodoc <73266859+DaphneGwyneth@users.noreply.github.com>
2023-07-05 22:14:04 +0800

    ok

--- RESLIST_FRAME.py
+++ RESLIST_FRAME.py
@@ -13,22 +13,24 @@ class ResListFrame(Frame):
         test.place(x=250, y=50)
 
         # Create the Treeview widget for the table
-        table = Treeview(self, columns=('Name', 'Date', 'Room Type', 'Price'))
-        table.column('#0', width=0, stretch=NO)
-        table.column('Name', anchor=W, width=150)
-        table.column('Date', anchor=CENTER, width=100)
-        table.column('Room Type', anchor=CENTER, width=100)
-        table.column('Price', anchor=CENTER, width=100)
+        self.table = Treeview(self, columns=('Room ID', 'Name', 'Date', 'Room Type', 'Price'))
+        self.table.column('#0', width=0, stretch=NO)
+        self.table.column('Room ID', anchor=W, width=100)
+        self.table.column('Name', anchor=W, width=100)
+        self.table.column('Date', anchor=CENTER, width=100)
+        self.table.column('Room Type', anchor=CENTER, width=100)
+        self.table.column('Price', anchor=CENTER, width=100)
 
         # Create table headings
-        table.heading('#0', text='', anchor=W)
-        table.heading('Name', text='Name', anchor=W)
-        table.heading('Date', text='Date', anchor=CENTER)
-        table.heading('Room Type', text='Room Type', anchor=CENTER)
-        table.heading('Price', text='Price', anchor=CENTER)
+        self.table.heading('#0', text='', anchor=W)
+        self.table.heading('Room ID', text='Room ID', anchor=W)
+        self.table.heading('Name', text='Name', anchor=W)
+        self.table.heading('Date', text='Date', anchor=CENTER)
+        self.table.heading('Room Type', text='Room Type', anchor=CENTER)
+        self.table.heading('Price', text='Price', anchor=CENTER)
 
         # table center
-        table.place(relx=0.4, rely=0.3, anchor='center')
+        self.table.place(relx=0.4, rely=0.3, anchor='center')
 
         # Read data from file and populate the table
         filepath = "list.txt"
@@ -36,13 +38,31 @@ class ResListFrame(Frame):
             with open(filepath, "r") as file:
                 for line in file:
                     data = line.strip().split(',')
-                    if len(data) == 4:
-                        name, date, room_type, price = data
-                        table.insert('', 'end', text=name, values=(name, date, room_type, price))
+                    print(data)  # Print data for debugging
+                    if len(data) == 5:
+                        room_id, name, date, room_type, price = data
+                        self.table.insert('', 'end', text=room_id, values=(room_id, name, date, room_type, price))
         except FileNotFoundError:
             print(f"File '{filepath}' not found.")
 
-        table.place(x=50, y=100)
+        self.table.place(x=50, y=100)
 
-        def update_table(self, name, date, room_type, price):
-            self.table.insert('', 'end', text=name, values=(name, date, room_type, price))
+    def update_table(self, name, date, room_type, price):
+        room_id = self.room_id_generator()  # Generate room_id
+        self.table.insert('', 'end', text=room_id, values=(room_id, name, date, room_type, price))
+
+    @staticmethod
+    def room_id_generator():
+        try:
+            with open("list.txt", "r") as file:
+                lines = file.readlines()
+                if lines:
+                    last_line = lines[-1].strip()
+                    last_room_id = int(last_line.split(',')[0])
+                    room_id = last_room_id + 1
+                else:
+                    room_id = 1
+        except FileNotFoundError:
+            room_id = 1
+
+        return room_id
