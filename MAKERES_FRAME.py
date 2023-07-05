a3bcc2331feeee93f387b9b531b0922d5920035b
Daphne Camodoc <73266859+DaphneGwyneth@users.noreply.github.com>
2023-07-06 01:18:27 +0800

    with calendar

--- MAKERES_FRAME.py
+++ MAKERES_FRAME.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter.ttk import Combobox
 from PIL import Image, ImageTk
 from MENUFRAME import *
+from tkcalendar import DateEntry
 
 class MakeResFrame(Frame):
     def __init__(self, parent, res_list_frame):
@@ -15,22 +16,22 @@ class MakeResFrame(Frame):
         label1=Label(self, text='Name', font=('tahoma', 12, 'bold'), fg='black')
         label1.place(x=350, y=130, anchor='center')
         textbox1=Entry(self, fg='black', font=('tahoma', 12))
-        textbox1.place(x=325, y=150)
+        textbox1.place(x=325, y=150, width=180, height=25)
 
-        label2=Label(self, text='Date (mm/dd/yy)', font=('tahoma', 12, 'bold'), fg='black')
-        label2.place(x=400, y=210, anchor='center')
-        textbox2=Entry(self, fg='black', font=('tahoma', 12))
-        textbox2.place(x=325, y=230)
+        label2 = Label(self, text='Date', font=('tahoma', 12, 'bold'), fg='black')
+        label2.place(x=350, y=210, anchor='center')
+        date_entry = DateEntry(self, font=('tahoma', 12), date_pattern='mm/dd/y')
+        date_entry.place(x=325, y=230)
 
         label3=Label(self, text='Room Type', font=('tahoma', 12, 'bold'), fg='black')
         label3.place(x=375, y=290, anchor='center')
         combo_box = Combobox(self)
         combo_box['values'] = ('Standard', 'Deluxe', 'Suite')
-        combo_box.place(x=395, y=320, anchor='center')
+        combo_box.place(x=415, y=320, anchor='center', width=180, height=25)
 
         def save_data():
             name = textbox1.get()
-            date = textbox2.get()
+            date = date_entry.get_date().strftime('%m/%d/%y')
             room_type = combo_box.get()
 
             price_mapping = {
@@ -41,7 +42,6 @@ class MakeResFrame(Frame):
             price = price_mapping.get(room_type, 0)
 
             filepath = "list.txt"
-            tempFile = "temp.txt"
 
             room_id = self.res_list_frame.room_id_generator()  # Generate room_id
 
@@ -54,5 +54,5 @@ class MakeResFrame(Frame):
             # Update the table in the ResListFrame
             self.res_list_frame.update_table(room_id, name, date, room_type, price)
 
-        button1 = Button(self, text='Add', fg='black', font=('tahoma', 14), command=save_data)
-        button1.place(x=385, y=380, anchor='center')
+        button1 = Button(self, text='Add', fg='white', font=('tahoma', 14), command=save_data, cursor='hand1',bg="#420C09")
+        button1.place(x=400, y=380, anchor='center', width=150)
